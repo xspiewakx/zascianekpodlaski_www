636d092acbfd4d281392ebaccbf28b6d74c69fde
Emil Stefaniuk <xspiewakx45@gmail.com>
2016-04-23 14:49:26 +0200

    master
    master

---
+++ js/hover_jq.js
@@ -0,0 +1,47 @@
+		$("#oferta1_img").removeClass("niewidok");
+		$("#oferta1_title").removeClass("niewidok");
+		$("#oferta1_txt").addClass("niewidok");
+		
+		$("#oferta2_img").addClass("niewidok");
+		$("#oferta2_title").addClass("niewidok");
+		$("#oferta2_txt").removeClass("niewidok");
+		
+		$("#oferta3_img").removeClass("niewidok");
+		$("#oferta3_title").removeClass("niewidok");
+		$("#oferta3_txt").addClass("niewidok");
+
+	function pokaz_tekst1() {
+		$("#oferta1_img").addClass("niewidok");
+		$("#oferta1_title").addClass("niewidok");
+		$("#oferta1_txt").removeClass("niewidok");
+}
+
+function schowaj_tekst1() {
+		$("#oferta1_img").removeClass("niewidok");
+		$("#oferta1_title").removeClass("niewidok");
+		$("#oferta1_txt").addClass("niewidok");
+}
+
+function pokaz_tekst2() {
+		$("#oferta2_img").addClass("niewidok");
+		$("#oferta2_title").addClass("niewidok");
+		$("#oferta2_txt").removeClass("niewidok");
+}
+
+function schowaj_tekst2() {
+		$("#oferta2_img").removeClass("niewidok");
+		$("#oferta2_title").removeClass("niewidok");
+		$("#oferta2_txt").addClass("niewidok");
+}
+
+function pokaz_tekst3() {
+		$("#oferta3_img").addClass("niewidok");
+		$("#oferta3_title").addClass("niewidok");
+		$("#oferta3_txt").removeClass("niewidok");
+}
+
+function schowaj_tekst3() {
+		$("#oferta3_img").removeClass("niewidok");
+		$("#oferta3_title").removeClass("niewidok");
+		$("#oferta3_txt").addClass("niewidok");
+}
